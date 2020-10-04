92d4f3377be276fc4443b5477390e9b6af3e9f83
Omar Monrroy <ogcm.kael@hotmail.com>
2020-10-03 21:24:00 -0500

    Creación de componentes adicionarles

--- src/app/contador/hijo/hijo.component.ts
+++ src/app/contador/hijo/hijo.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-hijo',
   templateUrl: './hijo.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class HijoComponent implements OnInit {
+  @Input() contador: number;
+  @Output() cambioContador = new EventEmitter<number>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  multiplicar() {
+    this.contador *= 2;
+    this.cambioContador.emit(this.contador);
   }
 
+  dividir() {
+    this.contador /= 2;
+    this.cambioContador.emit(this.contador);
+  }
 }
